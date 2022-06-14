19bd3ea2a85d6091ae0202afeaa2194a2537a15f
bipinacharya69 <bipinacharya284@gmail.com>
2022-06-14 23:01:19 +0545

    Complete form of hang-man

--- graphics_basics.cpp
+++ graphics_basics.cpp
@@ -1,10 +1,97 @@
+
 #include<iostream>
 #include<conio.h>
+#include<graphics.h>
 
-using namespace std;
+    // Drawing the bar 
+    void draw_bar()
+    {
+    line(20,20,getmaxx()-20,20);
+    line(getmaxx()/2,20,getmaxx()/2,getmaxy()/6);
+    }
+
+    // Drawing head of the man
+    void draw_head()
+    {
+    circle(getmaxx()/2,getmaxy()/6+60,60);
+    }
+
+    // drawing the body of man
+    void draw_body()
+    {
+    line( getmaxx()/2,getmaxy()/6+120,getmaxx()/2,getmaxy()-80);
+    }
+    
+    // drawing left hand
+    void draw_lhand()
+    {
+    line(getmaxx()/2,getmaxy()/6+120+40,getmaxx()/2+30,getmaxy()/6+120+80);
+    }
+
+    //drawing right hand
+    void draw_rhand()
+    {
+    line(getmaxx()/2,getmaxy()/6+120+40,getmaxx()/2-30,getmaxy()/6+120+80);
+    }
+
+    // drawing left leg
+    void draw_lleg()
+    {
+    line(getmaxx()/2,getmaxy()-80,getmaxx()/2+30,getmaxy()-80+30);
+
+    }
+
+    //drawing right leg
+    void draw_rleg()
+    {
+    line(getmaxx()/2,getmaxy()-80,getmaxx()/2-30,getmaxy()-80+30);
+    }
+
 
 int main()
 {
+    int gd=DETECT,gm;
+    //Initializing the graphs
+    initgraph (&gd,&gm,(char*)"");
+    
+    //For styling the lines
+    setcolor(GREEN);
+    setlinestyle(0, 0, 3);
+    
+    //styling text
+    settextstyle(8,0,3);
+
+    //printing text in the screen
+    outtextxy(getmaxx()/2-200, getmaxy()/2-50, "Welcome to hang-man Demo");
+   
+    settextstyle(8,0,1);
+    outtextxy(getmaxx()/2-150, getmaxy()/2-10, "Press space to continue..");
+    
+
+    //checking if the space is pressed
+    if(getch()==32)
+    {
+        // clearing the viewport
+        clearviewport();
+
+        // calling the functions
+        draw_bar();
+        delay(1000);
+        draw_head();
+        delay(1000);
+        draw_body();
+        delay(1000);
+        draw_rhand();
+        delay(1000);
+        draw_lhand();
+        delay(1000);
+        draw_rleg();
+        delay(1000);
+        draw_lleg();
+        
+    }
+
+   
     getch();
     return 0;
 }
