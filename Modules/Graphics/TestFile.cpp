eab488ab52da8178a932923733c97a51bc3c5e28
[sudip] <[koiralasudip580@gmail.com]>
2022-06-17 21:45:21 +0545

    hangman without delay and loop

--- Modules/Graphics/TestFile.cpp
+++ Modules/Graphics/TestFile.cpp
@@ -1,5 +1,25 @@
 #include<iostream>
 #include<conio.h>
+#include<graphics.h>
 using namespace std;
 
-// This file is just uploaded for testing
+int main()
+{
+    int gd = DETECT, gm;
+    // initgraph initializes the
+    initgraph(&gd, &gm, "");
+  
+    // To keep track of lines
+  
+       // setlinestyle function
+       //setlinestyle(DOTTED_LINE, 5, 0);
+       // Drawing line
+       line(339, 379 , 339, 349);
+       line(639, 0, 539, 0);
+       line(639, 0 , 639, 479);
+
+    getch();
+  
+    return 0;
+}
+
